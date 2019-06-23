1f9592966cd4f2ff0b2afd8b2f72d33c985732be
Asela Boteju <asela.boteju@gmail.com>
2019-06-22 21:13:26 -0400

    Removed multiple clicks, Background img, PDFs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ for (var i = 0; i < 10; i++){
 }
 
 var topics =[];
+var topicsClicked = [];
 
 function generateItemElement(item) {
   return `
@@ -22,7 +23,7 @@ function generateAnswerItemsString(answerList) {
   console.log("Generating answer list element");
 
   const items = answerList.map((item) => generateItemElement(item));
-  
+
   return items.join("");
 }
 
@@ -44,15 +45,22 @@ function handleIncorrectClick () {
   $('.js-answers-list').on('click', '.js-item-incorrect', event => {
     console.log('`handleIncorrectClicked` ran');
     const id = getItemIdFromElement(event.currentTarget);
-    topics.push(generateTopicElement(STORE[id-1]));
+    if (topicsClicked.some(function hasTopic(element) {
+      return element === id
+    })){
+
+    }
+    else {
+      topicsClicked.push(id)
+      topics.push(generateTopicElement(STORE[id-1]));}
     renderTopicsList(topics);
   });
 }
 
 function generateTopicElement(item) {
   return `
-    <li answerItemId= item>
-      <span class="inline"> ${item} </span>
+    <li answerItemId= ${item.topic}>
+      <span class="inline"> ${item.topic} <a href="https://asela-boteju.github.io/test-check/pdfs/${item.pdf}.pdf">Topic</a> </span>
     </li>`;
 }
 
@@ -65,6 +73,7 @@ function generateTopicsItemsString(topicsList) {
 function renderTopicsList(id) {
   // insert that HTML into the DOM
   $('.js-topics-list').html(id);
+  console.log(id)
 }
 
 function handleAnswerList() {
@@ -73,4 +82,3 @@ function handleAnswerList() {
 }
 
 $(handleAnswerList)
-
--- topicstore.js
+++ topicstore.js
@@ -1,11 +1,11 @@
 const STORE = [
-  'topic one',
-  'topic two',
-  'topic three',
-  'topic four',
-  'topic five',
-  'topic six',
-  'topic seven',
-  'topic eight',
-  'topic nine',
-  'topic ten']
+  {topic: 'topic one', pdf: 'pdfone'},
+  {topic: 'topic two', pdf: 'pdftwo'},
+  {topic: 'topic three', pdf: 'pdfthree'},
+  {topic: 'topic four', pdf: 'pdffour'},
+  {topic: 'topic five', pdf: 'pdffive'},
+  {topic: 'topic six', pdf: 'pdfsix'},
+  {topic: 'topic seven', pdf: 'pdfseven'},
+  {topic: 'topic eight', pdf: 'pdfeight'},
+  {topic: 'topic nine', pdf: 'pdfnine'},
+  {topic: 'topic ten', pdf: 'pdften'}]
